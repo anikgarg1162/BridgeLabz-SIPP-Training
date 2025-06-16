ea8a15ef24cd2565988a84c308382023405cd04b
anikgarg1162 <anikgarg6@gmail.com>
2025-06-16 11:54:27 +0530

    [Anik Garg].Add Problems For Control Flows On Day3

---
+++ ControlFlows/Divide_By_five.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Divide_By_five{
+	public static void main(String[] args)
+	{
+	 Scanner sc=new Scanner(System.in); 
+	 int n=sc.nextInt();	 
+	   if(n%5==0){
+		System.out.print("Number is divisible by 5");
+	    }
+	   else{
+		System.out.print("Number is not divisible by 5"); 
+	    }
+
+}
+}
+	 
+	
