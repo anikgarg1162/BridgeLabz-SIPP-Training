2c25abaefc6bc505be182cb3fc39f0ee59c65459
anikgarg1162 <anikgarg6@gmail.com>
2025-06-23 12:15:19 +0530

    Cleaned up main branch, kept hidden files like .gitignore

--- JavaFundamentals/Km_to_miles.java
+++
@@ -1,7 +0,0 @@
-public class Km_to_miles{
-    public static void main(String[] args){
-     double distance = 10.8;
-     double ans= distance / 1.6 ;
-     System.out.println("The distance 10.8 km in miles is " + ans);  
-}
-}
--- PracticeProblem/AreaOfCircle.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class AreaOfCircle{
-   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    double radius=sc.nextDouble();
-    double area= Math.PI * (radius*radius);
-  System.out.println(area);
-}
-}
